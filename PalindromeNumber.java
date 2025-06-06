9b42cd6a8c80f36c2aee7f32077212a03e73b2fb
lucarezende <lucasoprezende@gmail.com>
2025-06-06 17:44:04 +0100

    Add PalindromeNumber solution

---
+++ PalindromeNumber.java
@@ -0,0 +1,21 @@
+public class PalindromeNumber {
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(121));
+    }
+
+    public static boolean isPalindrome(int x) {
+       if (x < 0) {
+           return false;
+       }
+       int resultado = 0;
+       int numeroOriginal = x;
+       while (x != 0) {
+           int resto = x % 10;
+           resultado = resultado * 10 + resto;
+           x = x / 10;
+       }
+       return numeroOriginal == resultado;
+    }
+
+}
