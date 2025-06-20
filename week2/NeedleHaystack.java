6a110b866f285e9dee02b001d4cfa600624ddd3e
lucarezende <lucasoprezende@gmail.com>
2025-06-20 11:50:35 +0100

    Add strStr solution

---
+++ week2/NeedleHaystack.java
@@ -0,0 +1,33 @@
+package week2;
+
+public class NeedleHaystack {
+
+    public static void main(String[] args) {
+        System.out.println(strStr("hello", "ll"));
+    }
+
+    public static int strStr(String haystack, String needle) {
+        if (needle.isEmpty()) {
+            return -1;
+        }
+
+        if (haystack.length() < needle.length()) {
+            return -1;
+        }
+
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            boolean match = true;
+            for (int j = 0; j < needle.length(); j++) {
+                if (haystack.charAt(i + j) != needle.charAt(j)) {
+                    match = false;
+                    break;
+                }
+            }
+            if (match) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+}
--- week2/ValidAnagram.java
+++ week2/ValidAnagram.java
@@ -1,11 +1,12 @@
 package week2;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class ValidAnagram {
 
     public static void main(String[] args) {
-        System.out.println(isAnagram("aacc", "ccac"));
+        System.out.println(isAnagramSorted("aacc", "ccac"));
     }
 
     public static boolean isAnagram(String s, String t) {
@@ -26,5 +27,15 @@ public class ValidAnagram {
         }
 
         return true;
+    }
+
+    public static boolean isAnagramSorted(String s, String t) {
+        char[] stringA = s.toCharArray();
+        char[] stringB = t.toCharArray();
+
+        Arrays.sort(stringA);
+        Arrays.sort(stringB);
+
+        return Arrays.equals(stringA, stringB);
     }
 }
